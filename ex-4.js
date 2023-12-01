a80e8a8107402cedc02c12500e50bd9748969bdc
mygit <vuttipat.rungrojbribun@gmail>
2023-12-01 17:51:22 +0700

    Dear:assignment

--- ex-4.js
+++ ex-4.js
@@ -2,5 +2,10 @@
 let restaurants = ["KFC", "MOMO PARADISE", "Nabezo"];
 let newRestaurants = [];
 // Start coding here
-
+let i = 0;
+newRestaurants = restaurants;
+while (i < restaurants.length) {
+  "Restaurant Name: " + newRestaurants[i];
+  i++;
+}
 console.log(newRestaurants);
--- ex-6.js
+++ ex-6.js
@@ -3,5 +3,9 @@ let companyName = "TechUp Thailand";
 let reversedCompanyName = "";
 
 // Start coding here
+reversedCompanyName = companyName;
+reversedCompanyName = reversedCompanyName.split("");
+reversedCompanyName = reversedCompanyName.reverse("");
+reversedCompanyName = reversedCompanyName.join("");
 
 console.log(reversedCompanyName);
